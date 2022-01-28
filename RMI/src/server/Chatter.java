d18b03d40c73d739360335ac0de66b9ec41159d5
Lucas Lacerda <lucaslacerda.jlbl@gmail.com>
2022-01-28 20:51:20 -0300

    Subindo server

---
+++ RMI/src/server/Chatter.java
@@ -0,0 +1,22 @@
+package server;
+
+import client.ChatClient3IF;
+
+public class Chatter {
+
+	public String name;
+	public ChatClient3IF client;
+	
+	public Chatter(String name, ChatClient3IF client){
+		this.name = name;
+		this.client = client;
+	}
+
+	public String getName(){
+		return name;
+	}
+	public ChatClient3IF getClient(){
+		return client;
+	}
+	
+}
